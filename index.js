a2a3a75ec3916906ef6c669280745e8433c4dcc8
BRENDON SERVICE <bservice19@gmail.com>
2018-02-28 21:46:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,9 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string){
-  
+  if(string.equals(string.toUpperCase()))
+  return "YES, INDEED"
+  if(string.equals(string.toLowerCase()))
+  return "I can\'t hear you"
+  if(string.equals(string.))
 }
